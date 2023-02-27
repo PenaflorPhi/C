4109a53f3903696cd86b643c1262809db1e02bce
Ángel Peñaflor <PenaflorPhi@gmail.com>
2023-02-26 22:22:58 -0600

    Fixed bug in both programs

--- Chapter3/01-Binary_search.c
+++ Chapter3/01-Binary_search.c
@@ -1,34 +1,35 @@
 #include <stdio.h>
-int binarySearch(int x, const int array[], int size_of_array);
+#define SIZE 10
+int binarySearch(int x, const int array[]);
 
 int main(int argc, char *argv[]) {
   int x = 4;
   int n = 10;
 
-  int array[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-  printf("%d\n", binarySearch(x, array, n));
+  int array[SIZE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  printf("%d\n", binarySearch(x, array));
 
   return 0;
 }
 
-int binarySearch(int x, const int array[], int n) {
+int binarySearch(int x, const int array[]) {
   int low, high, mid;
   low  = 0;
-  high = n - 1;
+  high = SIZE - 1;
   mid  = (low + high) / 2;
 
-  while (low < high && x != array[mid]) {
-    if (x < mid) {
-      low = mid - 1;
+  while (low <= high && x != array[mid]) {
+    if (x > array[mid]) {
+      low = mid + 1;
     } else {
-      high = mid + 1;
+      high = mid - 1;
     }
+    mid = (low + high) / 2;
   }
+
   if (x == array[mid]) {
     return mid;
   } else {
     return -1;
   }
-
-  return -1;
 }
--- Chapter3/BinarySearch.c
+++ Chapter3/BinarySearch.c
@@ -1,81 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define SIZE 3000000
+#define SIZE 1000
 
-int  binsearch(int x, const int v[]);
+// Arrays
 int  trivialsearch(int x, const int array[]);
-void randomPopulate(int array[]);
+void randomArray(int array[]);
 void printarray(const int array[]);
 
+// Binary Search
+int binarySearch(int x, const int array[]);
+
 // Sorting
-void swap(int* a, int* b);
+void swap(int *a, int *b);
 void quicksort(int arr[], int low, int high);
-int partition(int arr[], int low, int high);
+int  partition(int arr[], int low, int high);
 
 int main(int argc, char *argv[]) {
   srand(time(NULL));
   long int x, n1, n2;
   x = rand() % SIZE;
   printf("x: %ld\n", x);
-  int    array[SIZE];
-  time_t begin, end;
+  int array[SIZE];
 
-  randomPopulate(array);
-	quicksort(array, 0, SIZE - 1);
+  randomArray(array);
+  quicksort(array, 0, SIZE - 1);
 
-  printf("DONE!\n");
-  begin = time(NULL);
-	printf("%ld\n", begin);
-  n1    = trivialsearch(x, array);
-  end   = time(NULL);
-  printf("Time: %ld , Position: %ld \n", begin - end, n1);
+  n1 = trivialsearch(x, array);
+  printf("Position: %ld \n", n1);
 
-  begin = time(NULL);
-  n2    = binsearch(x, array);
-  end   = time(NULL);
-  printf("Time: %ld, Position: %ld\n", begin - end, n2);
-	printf("%ld\n", end);
+  n2 = binarySearch(x, array);
+  printf("Position: %ld\n", n2);
 
   return 0;
 }
 
-/*randomPopulate*/
-void randomPopulate(int array[]) {
+void randomArray(int array[]) {
   for (long int i = 0; i < SIZE; ++i) {
     array[i] = rand() % SIZE;
   }
 }
 
-void swap(int* a, int* b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a       = *b;
+  *b       = temp;
 }
 
 int partition(int arr[], int low, int high) {
-    int pivot = arr[high];
-    int i = low - 1;
+  int pivot = arr[high];
+  int i     = low - 1;
 
-    for (int j = low; j <= high - 1; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            swap(&arr[i], &arr[j]);
-        }
+  for (int j = low; j <= high - 1; j++) {
+    if (arr[j] < pivot) {
+      i++;
+      swap(&arr[i], &arr[j]);
     }
+  }
 
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
+  swap(&arr[i + 1], &arr[high]);
+  return (i + 1);
 }
 
 void quicksort(int arr[], int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high);
+  if (low < high) {
+    int pi = partition(arr, low, high);
 
-        quicksort(arr, low, pi - 1);
-        quicksort(arr, pi + 1, high);
-    }
+    quicksort(arr, low, pi - 1);
+    quicksort(arr, pi + 1, high);
+  }
 }
+
 /*Trivialsearch*/
 int trivialsearch(int x, const int array[]) {
   for (long int i = 0; i < SIZE; ++i) {
@@ -84,29 +79,31 @@ int trivialsearch(int x, const int array[]) {
   return -1;
 }
 
-/*binsearch: find x in v[] <= v[1] <= ... <= v[n-1] */
-int binsearch(int x, const int v[]) {
-  long int low, high, mid;
-
+int binarySearch(int x, const int array[]) {
+  int low, high, mid;
   low  = 0;
   high = SIZE - 1;
-  while (low <= high) {
-    mid = (low + high) / 2;
-    if (x < v[mid]) {
-      high = mid - 1;
-    } else if (x > v[mid]) {
+  mid  = (low + high) / 2;
+
+  while (low <= high && x != array[mid]) {
+    if (x > array[mid]) {
       low = mid + 1;
     } else {
-      return mid;
+      high = mid - 1;
     }
+    mid = (low + high) / 2;
   }
 
-  return -1;
+  if (x == array[mid]) {
+    return mid;
+  } else {
+    return -1;
+  }
 }
 
-void printarray(const int array[]){
-	for(long int i = 0; i < SIZE; ++i){
-		printf("%d ", array[i]);
-	}
-	printf("\b\n");
+void printarray(const int array[]) {
+  for (long int i = 0; i < SIZE; ++i) {
+    printf("%d ", array[i]);
+  }
+  printf("\n");
 }
